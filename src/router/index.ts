2bb114d166ff1bc5d535bc7ad038b30fd7a5ea70
xmo <18851989097@163.com>
2022-03-21 09:22:10 +0800

    fix: 🐛 导出router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./route";
 
-export default createRouter({
+const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHistory(),
   routes, // `routes: routes` 的缩写
 });
+
+export default router;

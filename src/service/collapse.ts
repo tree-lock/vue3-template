c5f665bf42321890089c59dd15467a99b2f49f2f
xmo <18851989097@163.com>
2022-03-23 10:22:09 +0800

    fix: 🐛 修正细节

--- src/service/collapse.ts
+++ src/service/collapse.ts
@@ -1,4 +1,12 @@
-export const isCollapse = ref<boolean>(false);
+import { throttle } from "./util";
+
+window.addEventListener(
+  "resize",
+  throttle(() => (isCollapse.value = document.body.scrollWidth <= 1040), 300),
+  false
+);
+
+export const isCollapse = ref<boolean>(document.body.scrollWidth <= 1040);
 export const changeCollapse = () => {
   isCollapse.value = !isCollapse.value;
 };
--- src/service/index.ts
+++ src/service/index.ts
@@ -2,4 +2,5 @@ import * as auth from "./auth";
 import * as dark from "./dark";
 import * as profile from "./profile";
 import * as collapse from "./collapse";
+
 export default { auth, dark, profile, collapse };

4918b37d11755115e163819c353bc4b770f13517
Xmo <18851989097@163.com>
2022-04-01 15:15:56 +0800

    docs: ✏️ 添加Windows会产生bug的提示

--- src/api/mock.ts
+++ src/api/mock.ts
@@ -1,5 +1,6 @@
 /** 使用mock模拟数据 */
 import Mock, { mock, Random } from "mockjs";
+// Windows开发可能会遇到VSCode报错 找不到"meta" 的问题，这是VSCode自己的bug，暂不知解决办法，但保存一下env.d.ts就能解决。
 const mockUrl = import.meta.env.VITE_BASE_API_URL;
 Mock.setup({
   timeout: "200-600",

d04a67c6c06e3fd74e77ef17bb0f56bb1bd29774
Xmo <18851989097@163.com>
2022-04-08 17:03:56 +0800

    docs: ✏️ 多余的注释

--- src/service/profile.ts
+++ src/service/profile.ts
@@ -1,6 +1,5 @@
 const profile = ref<User.Response.Profile>();
 export default profile;
-// oi.init 返回的函数反复调用也只会执行一次
 export const getProfile = async () => {
   const res = await api.user.getProfileReq();
   profile.value = res.data;
